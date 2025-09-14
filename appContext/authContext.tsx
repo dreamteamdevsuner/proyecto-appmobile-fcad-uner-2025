fd1182eda14180c0ed83822d6553c89bd2cac2fe
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-09-14 15:26:30 -0300

    context

---
+++ appContext/authContext.tsx
@@ -0,0 +1,77 @@
+import { createContext, PropsWithChildren, useState } from "react";
+//PROVISORIO HAY QUE VER QUE INFO DEVUELVE LA DB CUANDO SE LOGEA USUARIO
+
+enum Role {
+  candidate = "candidate",
+  recruiter = "recruiter",
+}
+interface User {
+  name: string;
+  email: string;
+  role: Role;
+}
+interface UserState {
+  isLogged: boolean;
+  user: User;
+}
+const userStateInitiaValues: UserState = {
+  isLogged: false,
+  user: {} as User,
+};
+const AuthContext = createContext<{
+  userState: UserState;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+}>({
+  userState: {
+    isLogged: false,
+    user: {},
+  } as UserState,
+  login: function (email: string, password: string) {},
+  logout: function () {},
+});
+
+const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [userState, setUserState] = useState<UserState>(userStateInitiaValues);
+
+  //MOCKUP LOGIN
+  const login = (email: string, password: string) => {
+    if (email === "dev@dev.com") {
+      setUserState({
+        isLogged: true,
+        user: { name: "dev", email: "dev@mail.com", role: Role.candidate },
+      });
+      return;
+    }
+    if (email === "dev@dev.com") {
+      setUserState({
+        isLogged: true,
+        user: {
+          name: "dev",
+          email: "recruiter@mail.com",
+          role: Role.recruiter,
+        },
+      });
+      return;
+    } else {
+      setUserState({
+        isLogged: false,
+        user: {} as User,
+      });
+    }
+  };
+  //MOCKUP LOGOUT
+  const logout = () => {
+    setUserState({
+      isLogged: false,
+      user: {} as User,
+    });
+  };
+  return (
+    <AuthContext.Provider value={{ userState, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
