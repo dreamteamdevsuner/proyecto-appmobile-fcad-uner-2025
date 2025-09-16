c7e6cc89fcda7e3257bad18c209ec02bef609a50
Leo <leo.rosas.994@gmail.com>
2025-09-16 19:43:41 -0300

    add profile component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { Button, DefaultTheme, PaperProvider, Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Navigator from "./navigator/Navigator";
-import AuthProvider from "./appContext/authContext";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { Button, DefaultTheme, PaperProvider, Text } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Navigator from './navigator/Navigator';
+import AuthProvider from './appContext/authContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function App() {
   return (
     //hardcodeando paleta custom , pasando blue como primary
     <GestureHandlerRootView>
       <AuthProvider>
         <NavigationContainer>
-          <PaperProvider
-            theme={{ ...DefaultTheme, colors: { primary: "blue" } }}
-          >
-            <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+          <PaperProvider>
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
               <Navigator></Navigator>
               {/* <View style={styles.contentWrapper}>
             <View style={styles.btnContainer}>
@@ -33,13 +31,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  contentWrapper: { flexDirection: "column", gap: 2 },
+  contentWrapper: { flexDirection: 'column', gap: 2 },
   btnContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
