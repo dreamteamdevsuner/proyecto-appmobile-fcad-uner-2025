6eb224dded3993c2729de04edd69872a66172c31
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2025-09-17 17:36:31 -0300

    Se corrigió KeyboardAvoidingView de Conversación

--- app/private/recruiter/screens/favoritos/FavoritosNavigator.tsx
+++ app/private/recruiter/screens/favoritos/FavoritosNavigator.tsx
@@ -4,30 +4,37 @@ import Favoritos from '.';
 import FavoritosOferta from '../favoritos-oferta';
 import ROUTES from '../../navigator/routes';
 import Conversacion from '../conversacion';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 const FavoritosStack = createNativeStackNavigator<PrivateStackParamList>();
 
 const FavoritosNavigator = () => (
-  <FavoritosStack.Navigator>
-    <FavoritosStack.Screen
-      name={ROUTES.RECRUITER_FAVORITOS}
-      component={Favoritos}
-      options={{ title: 'Favoritos' }}
-    />
-    <FavoritosStack.Screen
-      name={ROUTES.RECRUITER_FAVORITOS_OFERTA}
-      component={FavoritosOferta}
-      options={({ route }) => ({
-        title: route.params?.title ?? 'Oferta',
-      })}
-    />
-    <FavoritosStack.Screen
-      name={ROUTES.RECRUITER_CONVERSACION}
-      component={Conversacion}
-      options={({ route }) => ({
-        title: route.params?.title ?? 'Conversación',
-      })}
-    />
-  </FavoritosStack.Navigator>
+  <KeyboardAvoidingView
+    style={{ flex: 1 }}
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    keyboardVerticalOffset={30}
+  >
+    <FavoritosStack.Navigator>
+      <FavoritosStack.Screen
+        name={ROUTES.RECRUITER_FAVORITOS}
+        component={Favoritos}
+        options={{ title: 'Favoritos' }}
+      />
+      <FavoritosStack.Screen
+        name={ROUTES.RECRUITER_FAVORITOS_OFERTA}
+        component={FavoritosOferta}
+        options={({ route }) => ({
+          title: route.params?.title ?? 'Oferta',
+        })}
+      />
+      <FavoritosStack.Screen
+        name={ROUTES.RECRUITER_CONVERSACION}
+        component={Conversacion}
+        options={({ route }) => ({
+          title: route.params?.title ?? 'Conversación',
+        })}
+      />
+    </FavoritosStack.Navigator>
+  </KeyboardAvoidingView>
 );
 export default FavoritosNavigator;
