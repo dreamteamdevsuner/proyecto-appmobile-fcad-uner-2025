f44ed2421ef3455f7c08e09c4aa8c11ae33833a7
Noelia <noeerivaas@gmail.com>
2025-09-23 21:27:00 -0300

    Seguridad de perfil

---
+++ app/private/shared/perfil/CambiarContasenaScreen.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { TextInput, Button, Dialog, Portal } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  actual: Yup.string().required("La contraseña actual es obligatoria"),
+  nueva: Yup.string().min(6, "Mínimo 6 caracteres").required("Nueva contraseña obligatoria"),
+  repetir: Yup.string()
+    .oneOf([Yup.ref("nueva")], "Las contraseñas no coinciden")
+    .required("Repite la nueva contraseña"),
+});
+
+export default function CambiarContrasenaScreen() {
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sectionTitle}>Creá una nueva contraseña</Text>
+
+      <Formik
+        initialValues={{ actual: "", nueva: "", repetir: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          setDialogVisible(true);
+          console.log("Cambio de contraseña:", values);
+        }}
+      >
+        {({ handleChange, handleSubmit, values, errors }) => (
+          <>
+            <TextInput
+              label="Contraseña actual"
+              value={values.actual}
+              onChangeText={handleChange("actual")}
+              secureTextEntry
+              style={styles.input}
+              error={!!errors.actual}
+              mode="outlined"
+              theme={{ roundness: 30 }}
+            />
+
+            <TextInput
+              label="Nueva contraseña"
+              value={values.nueva}
+              onChangeText={handleChange("nueva")}
+              secureTextEntry
+              style={styles.input}
+              error={!!errors.nueva}
+              mode="outlined"
+              theme={{ roundness: 30 }}
+            />
+
+            <TextInput
+              label="Repetir nueva contraseña"
+              value={values.repetir}
+              onChangeText={handleChange("repetir")}
+              secureTextEntry
+              style={styles.input}
+              error={!!errors.repetir}
+              mode="outlined"
+              theme={{ roundness: 30 }}
+            />
+
+            <Button mode="contained" style={styles.button} onPress={() => handleSubmit()}>
+              Guardar contraseña
+            </Button>
+            <Portal>
+              <Dialog
+                visible={dialogVisible}
+                onDismiss={() => setDialogVisible(false)}
+                style={{ borderRadius: 20}}
+              >
+                <Dialog.Title>Contaseña actualizada</Dialog.Title>
+                <Dialog.Content>
+                  <Text>La contraseña de tu cuenta ha sido actualizados correctamente.</Text>
+                </Dialog.Content>
+                <Dialog.Actions>
+                  <Button onPress={() => setDialogVisible(false)}>OK</Button>
+                </Dialog.Actions>
+              </Dialog>
+            </Portal>
+          </>
+        )}
+      </Formik>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1,
+     backgroundColor: "#fff", 
+    padding: 16
+  },
+  sectionTitle: { fontSize: 16, 
+    fontWeight: "bold", 
+    marginTop: 5,
+    marginVertical: 20,
+    textAlign: "center"
+  },
+  input: { 
+    marginHorizontal: 30,
+    marginBottom: 10,
+    backgroundColor: 'white',
+   },
+  button: { margin: 40,
+    marginVertical: 200,
+    backgroundColor: 'black',
+   },
+});
