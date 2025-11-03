91610cbf74f421d3c2a60ea56c098002360ba65d
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-11-03 00:03:50 -0300

    Add realtime listener for user profile updates
    Introduces a Supabase realtime channel to listen for updates to the logged-in user's profile in the 'usuario' table. Cleans up the listener on component unmount or when the user changes.

--- navigator/Navigator.tsx
+++ navigator/Navigator.tsx
@@ -8,6 +8,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { supabase } from '../supabase/supabaseClient';
 import { getUser, SecureStoreItem } from '@utils/secure-store';
 import { setItemAsync } from 'expo-secure-store';
+import { RealtimeChannel } from '@supabase/supabase-js';
 
 //Agregar Root Stack Params Luego
 const Stack = createNativeStackNavigator();
@@ -43,15 +44,40 @@ const Navigator = () => {
         }
       },
     );
+
     return () => {
       return onAuthStateSubscription.subscription.unsubscribe();
     };
   }, []);
 
   useEffect(() => {
+    let loggedUserUpdatesListener: RealtimeChannel;
     if (state.user) {
+      console.log('listening');
       SplashScreen.hideAsync();
+      // Assuming a 'profiles' table with user-specific data
+      loggedUserUpdatesListener = supabase
+        .channel('public:usuario')
+        .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+            table: 'usuario',
+            filter: `id=eq.${state.user.id}`,
+          },
+          (payload) => {
+            console.log('Profile updated:', payload.new);
+            // Update UI or application state with new profile data
+          },
+        )
+        .subscribe();
     }
+    return () => {
+      if (loggedUserUpdatesListener) {
+        loggedUserUpdatesListener.unsubscribe();
+      }
+    };
   }, [state.user]);
 
   return state.user ? (
