518f357064b667313eaf4fe70adabf499b885665
Den Durruty <dendurruty@users.noreply.github.com>
2025-11-25 18:59:50 -0300

    Press notificaciones lado reclutador devuelve usuarios matcheados por oferta

--- app/private/recruiter/screens/notificaciones/NotificationsProfile.tsx
+++ app/private/recruiter/screens/notificaciones/NotificationsProfile.tsx
@@ -35,17 +35,14 @@ export default function NotificationsProfile() {
   }, [state.user?.id]);
 
   const handlePress = async (notif: any) => {
-    // 🔹 Actualizamos visualmente antes de la llamada
     setNotifications((prev) =>
       prev.map((n) =>
         n.id === notif.id ? { ...n, idestadonotificacion: 3 } : n,
       ),
     );
 
-    // 🔹 Marcamos como leída en la base
     await markNotificationAsRead(notif.id);
 
-    // 🔹 Navegamos según tipo
     switch (notif.tipo) {
       case 'match':
         navigation.navigate(ROUTES.RECRUITER_FAVORITOS_OFERTA, {
